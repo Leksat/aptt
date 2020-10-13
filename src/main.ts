e15f2a87cfb3d6cc72a8966eea5abf976d37bbd0
Alex Tkachev <alex.leksat@gmail.com>
2020-10-13 16:56:02 +0300

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@ if (require('electron-squirrel-startup')) {
 }
 
 let mainWindow: BrowserWindow;
-const createWindow = (): void => {
-  // Create the browser window.
+
+app.on('ready', () => {
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
@@ -25,36 +25,10 @@ const createWindow = (): void => {
     },
     skipTaskbar: true,
   });
-
-  // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
   setTimeout(() => mainWindow.webContents.send('bob', 'okay'), 2000);
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
 });
 
 let tray: Tray;
@@ -93,7 +67,8 @@ app.whenReady().then(() => {
   }, 3000);
 });
 
-app.dock.hide();
+app.on('window-all-closed', () => {
+  // Empty handler is required to disable auto-exit.
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+app.dock.hide();
