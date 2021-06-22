9eb75bbcd19beabbe8c42ebdfbe350460141cafa
Alex Tkachev <alex.leksat@gmail.com>
2021-06-22 16:36:06 +0300

    fix jira ticket regex

--- src/shared/tickets.ts
+++ src/shared/tickets.ts
@@ -4,4 +4,4 @@ export const getTicketFromClipboard = (): string =>
   parseTicket(clipboard.readText('clipboard'));
 
 export const parseTicket = (text: string): string =>
-  text.match(/[A-Z]+-\d+/)?.[0] || '';
+  text.match(/[A-Z0-9][A-Z0-9]*-\d+/)?.[0] || '';
