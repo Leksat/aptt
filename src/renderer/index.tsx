70c068c1db8401d6e06789e55811a21bd356e25f
Alex Tkachev <alex.leksat@gmail.com>
2022-08-23 15:50:16 +0300

    chore: get ready for aptt 3.0

--- src/renderer/index.tsx
+++
@@ -1,6 +0,0 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactApp from './ReactApp';
-
-ReactDOM.render(<ReactApp />, document.getElementById('root'));
--- src/shared/tickets.ts
+++
@@ -1,7 +0,0 @@
-import { clipboard } from 'electron';
-
-export const getTicketFromClipboard = (): string =>
-  parseTicket(clipboard.readText('clipboard'));
-
-export const parseTicket = (text: string): string =>
-  text.match(/[A-Z0-9][A-Z0-9]*-\d+/)?.[0] || '';
