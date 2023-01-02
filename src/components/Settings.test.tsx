cabd41183eabb38934cf28da75131c61d54fd565
Alex Tkachev <alex.leksat@gmail.com>
2023-01-02 13:45:23 +0300

    chore: migrate to pnpm and fix tests

---
+++ src/components/Settings.test.tsx
@@ -0,0 +1,35 @@
+import { describe, expect, test } from 'vitest';
+import { Settings } from './Settings';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { mockIPC } from '@tauri-apps/api/mocks';
+import '@testing-library/jest-dom';
+import { store } from '../lib/store';
+
+beforeEach(() => {
+  store.clear();
+});
+
+describe('Settings', () => {
+  test('Basics', async () => {
+    mockIPC(() => null);
+    const { unmount } = render(<Settings close={() => {}} />);
+
+    await userEvent.type(screen.getByLabelText(/Jira Account ID/), 'AccountId');
+    await userEvent.type(screen.getByLabelText(/Tempo API token/), 'Token');
+    await userEvent.click(screen.getByRole('button', { name: 'Close' }));
+    expect(store.get('jira').workerId).toBe('');
+    expect(store.get('jira').token).toBe('');
+
+    unmount();
+
+    render(<Settings close={() => {}} />);
+    expect(screen.getByLabelText(/Jira Account ID/)).toHaveValue('');
+    expect(screen.getByLabelText(/Tempo API token/)).toHaveValue('');
+    await userEvent.type(screen.getByLabelText(/Jira Account ID/), 'AccountId');
+    await userEvent.type(screen.getByLabelText(/Tempo API token/), 'Token');
+    await userEvent.click(screen.getByRole('button', { name: 'Save' }));
+    expect(store.get('jira').workerId).toBe('AccountId');
+    expect(store.get('jira').token).toBe('Token');
+  });
+});
